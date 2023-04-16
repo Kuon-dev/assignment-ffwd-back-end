1deea4fdecee377917b639ab693167cf23793482
HoKaeLok <hokaelok123@gmail.com>
2023-04-16 17:21:17 +0800

    feat update: added comments functions

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use App\Models\Forum;
 use Illuminate\Http\Request;
 
 class ForumController extends Controller
@@ -10,18 +12,19 @@ class ForumController extends Controller
     //   FORUM SECTION   //
     // get all forum post
     public function index() {
-        // $forums = Forum::latest()->get();
+        $forums = Forum::latest()->get();
         
-        // return view('pizzas.index', [
-        //     'pizzas' => $pizzas,
-        // ]);
+        return view('forums.index', [
+            'forums' => $forums,
+        ]);
     }
 
-    // get specific forum post
+    // get specific forum post with comments
     public function show($forumID) {
-        // $forum = Forum::findOrFail($forumID);
+        $forum = Forum::findOrFail($forumID);
+        $comments = Comment::findOrFail($forumID);
 
-        // return view('forum.show', ['forum' => $forum]);
+        return view('forum.show', ['forum' => $forum, 'comments' => $comments]);
     }
 
     // create new forum
@@ -31,22 +34,37 @@ class ForumController extends Controller
 
     // edit forum
     public function edit($forumID) {
-        // $forum = Forum::findOrFail($forumID);
+        $forum = Forum::findOrFail($forumID);
 
-        // return view('forum.edit', compact('forum'));
+        return view('forum.edit', compact('forum'));
     }
 
     // delete forum
     public function destroy($forumID) {
-        // $forum = Forum::findOrFail($forumID);
-        // $forum->delete();
+        $forum = Forum::findOrFail($forumID);
+        $forum->delete();
 
         // return redirect('someOtherPage');
     }
 
     //   COMMENT SECTION   //
-    // 
-    public function showComment($forumID) {
-        
+    // create new comment
+    public function createComment($forumID) {
+
+    }
+
+    // edit comment
+    public function editComment($commentID) {
+        $comment = Comment::findOrFail($commentID);
+
+        return view('comment.edit', compact('comment'));
+    }
+
+    // delete comment
+    public function destroyComment($commentID) {
+        $comment = Comment::findOrFail($commentID);
+        $comment->delete();
+
+        // return redirect('someOtherPage');
     }
 }
