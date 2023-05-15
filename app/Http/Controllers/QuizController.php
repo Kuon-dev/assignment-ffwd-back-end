a2b835226224b7a3090ed64c3e6722387e3bb36e
Char Lap Chun <91192122+AsianAsh@users.noreply.github.com>
2023-05-16 02:45:24 +0800

    Feat: Added Route & Function to get Specific Quiz Record

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -7,51 +7,58 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
-class QuizController extends Controller{
-    // get all quizzes of the current user (user_id) 
-    public function index(Request $request) {
-        $userID = $request->user;
-        $title = $request->title;
-    
-        $quizzes = Quiz::where("user_id", $userID)
-            ->where("title", "like", "%{$title}%")
-            ->orderByDesc("attempted_date")
-            ->latest()
-            ->skip($request->index * 10)
-            ->take(10)
-            ->get();
-    
-        // $quizzesCount = Quiz::where("user_id", $userID)
-        //     ->where("title", "like", "%{$title}%")
-        //     ->count();
-    
-        return response()->json([
-            "data" => $quizzes,
-            // "quizzes_count" => $quizzesCount,
-        ]);
-    }
+class QuizController extends Controller {
+  // get all quizzes of the current user (user_id)
+  public function index(Request $request) {
+    $userID = $request->user;
+    $title = $request->title;
 
-    public function create(Request $request) {
-        Log::debug($request);
-        $newQuizScore = Quiz::create([
-            "title" => $request->title,
-            "user_id" => $request->user_id,
-            "score" => $request->score,
-            "attempted_date" => $request->attempted_date,
-            "completed_time" => $request->completed_time,
-        ]);
-        return response()->json(
-            ["message" => "Quiz Score Record has been Created."],
-            200
-        );
-    }
+    $quizzes = Quiz::where("user_id", $userID)
+      ->where("title", "like", "%{$title}%")
+      ->orderByDesc("attempted_date")
+      ->latest()
+      ->skip($request->index * 10)
+      ->take(10)
+      ->get();
 
-    // Get single score based on id
-    public function getScore(Request $request) {
-        $quizScore= Quiz::where("id", $request->score_id)->get();
-        return response()->json([
-            "score" => $quizScore,
-        ]);
+    // $quizzesCount = Quiz::where("user_id", $userID)
+    //     ->where("title", "like", "%{$title}%")
+    //     ->count();
 
-    }
+    return response()->json([
+      "data" => $quizzes,
+      // "quizzes_count" => $quizzesCount,
+    ]);
+  }
+
+  public function create(Request $request) {
+    Log::debug($request);
+    $newQuizScore = Quiz::create([
+      "title" => $request->title,
+      "user_id" => $request->user_id,
+      "score" => $request->score,
+      "attempted_date" => $request->attempted_date,
+      "completed_time" => $request->completed_time,
+    ]);
+    return response()->json(
+      ["message" => "Quiz Score Record has been Created."],
+      200
+    );
+  }
+
+  // Get single score based on id
+  public function getScore(Request $request) {
+    $quizScore = Quiz::where("id", $request->score_id)->get();
+    return response()->json([
+      "score" => $quizScore,
+    ]);
+  }
+
+  // get specific quiz record details based on quiz_id
+  public function show(Request $request) {
+    $quiz = Quiz::where("id", $request->quiz_id)->get();
+    return response()->json([
+      "quiz" => $quiz[0],
+    ]);
+  }
 }
