4250311e4d6e7dfb471380e68b1921d43e0324d6
Kuon-dev <aaronlyn88@gmail.com>
2023-05-20 20:20:11 +0800

    refactor: changed frontend fetching contents into backend

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -7,6 +7,7 @@ use App\Models\Quiz;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class QuizController extends Controller {
   // get all quizzes of the current user (user_id)
@@ -94,5 +95,9 @@ class QuizController extends Controller {
       "data" => $quizzes,
       "users" => $userNames,
     ]);
+  }
+
+  public function getCourse($id){
+    return response((Storage::disk('local')->get('/public/' . $id)));
   }
 }
